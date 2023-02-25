a700c2660004fb3a32b15d3f3a2b3f62421aaf11
Hilal2013 <hilalozkul@gmail.com>
2023-02-24 23:48:31 -0500

    added Ingredients file

---
+++ spring-lab-Yummy/src/main/java/com/cydeo/model/Ingredients.java
@@ -0,0 +1,11 @@
+package com.cydeo.model;
+
+import com.cydeo.QuantityType;
+
+import java.math.BigDecimal;
+
+public class Ingredients {
+    private String name;
+    private BigDecimal quantity;
+    private QuantityType quantityType;
+}
