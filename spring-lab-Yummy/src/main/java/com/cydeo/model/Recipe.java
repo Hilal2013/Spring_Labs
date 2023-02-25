34086318c49a37b48e0036c484589c2eee59f0a4
Hilal2013 <hilalozkul@gmail.com>
2023-02-24 23:54:47 -0500

    updated

--- spring-lab-Yummy/src/main/java/com/cydeo/model/Recipe.java
+++ spring-lab-Yummy/src/main/java/com/cydeo/model/Recipe.java
@@ -1,4 +1,14 @@
 package com.cydeo.model;
 
+import com.cydeo.RecipeType;
+
+import java.util.UUID;
+
 public class Recipe {
+    private UUID id;
+    private String name;
+    private int duration;//cooking time
+    private String preparation;
+    private Ingredients ingredients;
+    private RecipeType recipeType;
 }
