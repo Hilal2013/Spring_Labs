9205882f4ef5da7615e27e1a7d30cea90791677f
Hilal2013 <hilalozkul@gmail.com>
2023-02-24 23:37:42 -0500

    added enums

---
+++ spring-lab-Yummy/src/main/java/com/cydeo/QuantityType.java
@@ -0,0 +1,5 @@
+package com.cydeo;
+
+public enum QuantityType {
+    OUNCE,LB,TBSP;
+}
---
+++ spring-lab-Yummy/src/main/java/com/cydeo/RecipeType.java
@@ -0,0 +1,6 @@
+package com.cydeo;
+
+public enum RecipeType {
+
+    BREAKFAST,LUNCH,DESERT,APPETIZER,BAKED,SOUP,VEGETARIAN
+}
