32cc727ab4fbe52e0a77de48481711bcfa3b8c68
Hilal2013 <hilalozkul@gmail.com>
2023-02-24 23:36:53 -0500

    updated

--- spring-lab-Yummy/src/main/java/com/cydeo/RecipeApplication.java
+++ spring-lab-Yummy/src/main/java/com/cydeo/RecipeApplication.java
@@ -1,13 +1,13 @@
-package com.cydeo.springlabyummy;
+package com.cydeo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringLabYummyApplication {
+public class RecipeApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringLabYummyApplication.class, args);
+        SpringApplication.run(RecipeApplication.class, args);
     }
 
 }
