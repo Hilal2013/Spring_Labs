60924eea58d341370ab7502292c10c4814abe5f0
Hilal2013 <hilalozkul@gmail.com>
2023-02-24 23:26:12 -0500

    created spring-lab-Yummy module

---
+++ spring-lab-Yummy/src/main/java/com/cydeo/springlabyummy/SpringLabYummyApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo.springlabyummy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLabYummyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringLabYummyApplication.class, args);
+    }
+
+}
---
+++ spring-lab-Yummy/src/test/java/com/cydeo/springlabyummy/SpringLabYummyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cydeo.springlabyummy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLabYummyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
