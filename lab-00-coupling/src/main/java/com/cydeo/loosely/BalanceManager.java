9256c32c5d74ecc7a3ede944e8169121b33c0460
Hilal2013 <hilalozkul@gmail.com>
2023-02-25 22:50:31 -0500

    updated

--- lab-00-coupling/src/main/java/com/cydeo/loosely/BalanceManager.java
+++ lab-00-coupling/src/main/java/com/cydeo/loosely/BalanceManager.java
@@ -1,10 +1,14 @@
 package com.cydeo.loosely;
 
+import java.math.BigDecimal;
+
 public class BalanceManager {
-    public boolean checkout(
-            // add method parameters
-    ){
-        // implement checkout business
+    public boolean checkout(Balance balance, BigDecimal productAmount){
+BigDecimal finalAmount=balance.getAmount().subtract(productAmount);
+        if(finalAmount.compareTo(BigDecimal.ZERO) > 0){
+            balance.setAmount(finalAmount);
+            return true;
+        }
         return false;
     }
 }
