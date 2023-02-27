2e5b7e4bed83c6d2dcfec4ba9a244f68c5030380
Hilal2013 <hilalozkul@gmail.com>
2023-02-26 19:09:04 -0500

    added files

---
+++ spring-lab-review-project/src/main/java/com/cydeo/repository/OvertimeHours.java
@@ -0,0 +1,11 @@
+package com.cydeo.repository;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class OvertimeHours implements HoursRepository{
+    @Override
+    public int getHours() {
+        return 15;
+    }
+}
