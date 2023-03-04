ec3af5d4fdfc8a4b30c9555cd211fd8d15f7d22b
Hilal2013 <hilalozkul@gmail.com>
2023-03-03 19:28:30 -0500

    created lab04 springMVC module

---
+++ lab-04-springmvc/src/main/java/com/cydeo/lab04springmvc/Lab04SpringmvcApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo.lab04springmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab04SpringmvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab04SpringmvcApplication.class, args);
+    }
+
+}
