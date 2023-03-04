d2b51635dc36860786a14505d0c596b40ab4885a
Hilal2013 <hilalozkul@gmail.com>
2023-03-03 23:10:42 -0500

    updated

--- lab-04-springmvc/src/main/java/com/cydeo/lab04springmvc/controller/ProductController.java
+++ lab-04-springmvc/src/main/java/com/cydeo/lab04springmvc/controller/ProductController.java
@@ -2,6 +2,7 @@ package com.cydeo.lab04springmvc.controller;
 
 import com.cydeo.lab04springmvc.service.ProductService;
 
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,8 +10,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@AllArgsConstructor
 public class ProductController {
-    @Autowired
+
     ProductService productService;
     @RequestMapping("/search-product/{name}")//http://localhost:8080/search-product/milk
     //http://localhost:8080/product/milk
