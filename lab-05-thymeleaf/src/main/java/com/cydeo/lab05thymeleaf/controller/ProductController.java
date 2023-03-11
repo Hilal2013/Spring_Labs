dc4fde883299363953c0b7af0523947375179ced
Hilal2013 <hilalozkul@gmail.com>
2023-03-10 20:18:09 -0500

    updated

--- lab-05-thymeleaf/src/main/java/com/cydeo/lab05thymeleaf/controller/ProductController.java
+++ lab-05-thymeleaf/src/main/java/com/cydeo/lab05thymeleaf/controller/ProductController.java
@@ -1,25 +1,43 @@
 package com.cydeo.lab05thymeleaf.controller;
 
+import com.cydeo.lab05thymeleaf.model.Product;
+import com.cydeo.lab05thymeleaf.service.ProductService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
-@RequestMapping("/product")
-public class ProductController {
-   @GetMapping
-    public String create(){
 
+public class ProductController {
+
+ private final   ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping("/create-product")
+    public String createProduct(Model model){
+
+model.addAttribute("product",new Product());
 
         return "product/create-product";
     }
+    @PostMapping("/create-form")
+    public String addProduct(){
 
-    @GetMapping
-    public String list(){
 
+        return "redirect:/create-product";
+    }
 
+    @GetMapping("/list")
+    public String list(Model model){
+
+model.addAttribute("productList",productService.listProduct());
         return "product/list";
     }
+
 
 
 }
