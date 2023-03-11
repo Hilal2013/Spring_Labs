e25fab1b87ef108c3018e283695f0f4dd5e53a12
Hilal2013 <hilalozkul@gmail.com>
2023-03-10 20:18:22 -0500

    updated

--- lab-05-thymeleaf/src/main/java/com/cydeo/lab05thymeleaf/controller/CartController.java
+++ lab-05-thymeleaf/src/main/java/com/cydeo/lab05thymeleaf/controller/CartController.java
@@ -1,17 +1,42 @@
 package com.cydeo.lab05thymeleaf.controller;
 
+import com.cydeo.lab05thymeleaf.service.CartService;
+import com.cydeo.lab05thymeleaf.service.impl.CartServiceImpl;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.UUID;
 
 @Controller
-@RequestMapping("/cart")
+
 public class CartController {
-    @GetMapping("/show")
-    public String showCart(){
+private  final CartService cartService;
 
+    public CartController(CartService cartService) {
+        this.cartService = cartService;
+    }
 
+    @GetMapping("/cart")
+    public String showCart(Model model){
+model.addAttribute("cartList",CartServiceImpl.CART.getCartItemList());
+model.addAttribute("cartTotalAmount", CartServiceImpl.CART.getCartTotalAmount());
         return "cart/show-cart";
+    }
+
+    @GetMapping("/addToCart/{productId}/{quantity}")
+    public String addToCart(@PathVariable UUID productId,@PathVariable Integer quantity){
+
+cartService.addToCart(productId,quantity);
+        return "redirect:/list";
+    }
+
+    @GetMapping("/delete/{productId}")
+    public String delete(@PathVariable UUID productId){
+cartService.deleteFromCart(productId);
+
+        return "redirect:/cart";
     }
 
 }
