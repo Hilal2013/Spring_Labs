6ba91faaab7a03dbbdfc0aa45ca5c15a34a9f29c
Hilal2013 <hilalozkul@gmail.com>
2023-03-10 20:43:06 -0500

    ProductServiceImpl

---
+++ lab-05-thymeleaf/src/main/java/com/cydeo/lab05thymeleaf/service/impl/ProductServiceImpl.java
@@ -0,0 +1,40 @@
+package com.cydeo.lab05thymeleaf.service.impl;
+
+
+import com.cydeo.lab05thymeleaf.model.Product;
+import com.cydeo.lab05thymeleaf.repository.ProductRepository;
+import com.cydeo.lab05thymeleaf.service.ProductService;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+    private final ProductRepository productRepository;
+
+    public ProductServiceImpl(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+    @Override
+    public boolean productCreate(Product product){
+        // todo implement method
+        product.setId(UUID.randomUUID());
+        return true;
+    }
+
+    @Override
+    public List<Product> listProduct() {
+        // todo implement method
+        return productRepository.findAll();
+    }
+
+    @Override
+    public Product findProductById(UUID uuid){
+        // todo implement method
+        return productRepository.findProductById(uuid);
+    }
+
+}
