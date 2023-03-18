27adcdd1887fa0dea5faf55fd81ec2999c9fab6f
Hilal2013 <hilalozkul@gmail.com>
2023-03-17 23:20:20 -0400

    updated

--- lab-05-thymeleaf/src/main/java/com/cydeo/lab05thymeleaf/service/impl/CartServiceImpl.java
+++ lab-05-thymeleaf/src/main/java/com/cydeo/lab05thymeleaf/service/impl/CartServiceImpl.java
@@ -1,6 +1,9 @@
 package com.cydeo.lab05thymeleaf.service.impl;
 
 import com.cydeo.lab05thymeleaf.model.Cart;
+import com.cydeo.lab05thymeleaf.model.CartItem;
+import com.cydeo.lab05thymeleaf.model.Product;
+import com.cydeo.lab05thymeleaf.repository.ProductRepository;
 import com.cydeo.lab05thymeleaf.service.CartService;
 import com.cydeo.lab05thymeleaf.service.ProductService;
 
@@ -18,17 +21,22 @@ public class CartServiceImpl implements CartService {
 
     public CartServiceImpl(ProductService productService) {
         this.productService = productService;
+
     }
 
     @Override
     public Cart addToCart(UUID productId, Integer quantity){
         //todo retrieve product from repository method
-
+        Product product = productService.findProductById(productId);
         //todo initialise cart item
-        //todo calculate cart total amount
+    CartItem cartItem= CART.getCartItemList().stream()
+                .filter(p->p.getProduct().getId().equals(productId))
+                .findAny().get();
 
+        //todo calculate cart total amount
+     cartItem.setTotalAmount(product.getPrice().multiply(BigDecimal.valueOf(cartItem.getQuantity())));
         //todo add to cart
-    //   CART.getCartItemList().add(cartItem);
+
         return CART;
     }
 
