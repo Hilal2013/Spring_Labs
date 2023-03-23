fd252ea008fbb109167e8231aeaaacf700bb351e
Hilal2013 <hilalozkul@gmail.com>
2023-03-23 14:29:18 -0400

    updated

--- spring-lab-review-project/src/main/java/com/cydeo/SalaryApp.java
+++ spring-lab-review-project/src/main/java/com/cydeo/SalaryApp.java
@@ -1,17 +1,14 @@
 package com.cydeo;
 
-import com.cydeo.config.SalaryConfig;
-import com.cydeo.service.SalaryService;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class SalaryApp {
     public static void main(String[] args) {
 
-        ApplicationContext container = new AnnotationConfigApplicationContext(SalaryConfig.class);
+        ApplicationContext container = new AnnotationConfigApplicationContext();
 
-        SalaryService salaryService = container.getBean(SalaryService.class);
 
-        salaryService.calculateRegularSalary();
     }
 }
