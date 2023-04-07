3721121c5c8e1902539d0c5064177880241b98b3
Hilal2013 <hilalozkul@gmail.com>
2023-04-07 19:58:53 -0400

    customer entity

---
+++ lab-06-orm-ecommerce/src/main/java/com/cydeo/entity/Customer.java
@@ -0,0 +1,31 @@
+package com.cydeo.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@NoArgsConstructor
+@Data
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String userName;
+@OneToMany(mappedBy = "customerId")
+    private List<Address> addressList;
+
+//private Balance balance;
+
+    public Customer(String email, String firstName, String lastName, String userName) {
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.userName = userName;
+    }
+}
