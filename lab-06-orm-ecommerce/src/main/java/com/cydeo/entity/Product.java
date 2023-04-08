9a8a40febc952d24edb456df123cddf2473bd495
Hilal2013 <hilalozkul@gmail.com>
2023-04-07 20:29:10 -0400

    Product entity

---
+++ lab-06-orm-ecommerce/src/main/java/com/cydeo/entity/Product.java
@@ -0,0 +1,29 @@
+package com.cydeo.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@NoArgsConstructor
+@Data
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Double price;
+    private Integer quantity;
+    private Integer remaining_quantity;
+    @ManyToMany(mappedBy = "productList")
+    private List<Category> categoryList;
+
+    public Product(String name, Double price, Integer quantity, Integer remaining_quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+        this.remaining_quantity = remaining_quantity;
+    }
+}
