0f1878dbcc36737273aeb50e89a3335a37a109ce
Hilal2013 <hilalozkul@gmail.com>
2023-04-07 20:42:08 -0400

    created lab07Cinema module

---
+++ lab-07-orm-cinema/src/main/java/com/cydeo/Lab07OrmCinemaApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab07OrmCinemaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab07OrmCinemaApplication.class, args);
+    }
+
+}
