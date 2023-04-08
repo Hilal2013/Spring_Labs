8094fcb0c417cc66987209e1be845238a3a09dc5
Hilal2013 <hilalozkul@gmail.com>
2023-04-08 13:10:15 -0400

    updated

--- lab-06-orm-ecommerce/src/main/java/com/cydeo/entity/Payment.java
+++ lab-06-orm-ecommerce/src/main/java/com/cydeo/entity/Payment.java
@@ -1,4 +1,26 @@
 package com.cydeo.entity;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+
+@Entity
+@NoArgsConstructor
+@Data
 public class Payment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private BigDecimal paidPrice;
+    private String paymentMethod;
+
+    public Payment(BigDecimal paidPrice, String paymentMethod) {
+        this.paidPrice = paidPrice;
+        this.paymentMethod = paymentMethod;
+    }
 }
