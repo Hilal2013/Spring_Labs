dd098d6968b27fa38f3e89cfb1d5c7192e37a722
Hilal2013 <hilalozkul@gmail.com>
2023-04-08 13:10:36 -0400

    updated

--- lab-06-orm-ecommerce/src/main/java/com/cydeo/entity/CartItem.java
+++ lab-06-orm-ecommerce/src/main/java/com/cydeo/entity/CartItem.java
@@ -3,18 +3,23 @@ package com.cydeo.entity;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.math.BigDecimal;
 
 @Entity
 @NoArgsConstructor
 @Data
-public class cartItem {
+public class CartItem {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private BigDecimal quantity;
+    @ManyToOne
+    @JoinColumn(name = "cart_id")
+    private Cart cart;
+    @ManyToOne
+    private Product product;
+    public CartItem(BigDecimal quantity) {
+        this.quantity = quantity;
+    }
 }
