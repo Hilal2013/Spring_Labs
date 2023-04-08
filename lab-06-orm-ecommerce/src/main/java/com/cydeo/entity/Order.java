b08b4e7df9856abaadf519b50976284a22a44a29
Hilal2013 <hilalozkul@gmail.com>
2023-04-08 13:10:19 -0400

    updated

--- lab-06-orm-ecommerce/src/main/java/com/cydeo/entity/Order.java
+++ lab-06-orm-ecommerce/src/main/java/com/cydeo/entity/Order.java
@@ -1,4 +1,29 @@
 package com.cydeo.entity;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@NoArgsConstructor
+@Data
+@Table(name = "orders")
 public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private BigDecimal paidPrice;
+    private BigDecimal totalPrice;
+    @OneToOne
+    private Cart cart;
+    @ManyToOne
+    private Customer customer;
+    @OneToOne
+    @JoinColumn(name="payment_id")
+    private Payment payment;
+    public Order(BigDecimal totalPrice) {
+        this.totalPrice = totalPrice;
+    }
 }
