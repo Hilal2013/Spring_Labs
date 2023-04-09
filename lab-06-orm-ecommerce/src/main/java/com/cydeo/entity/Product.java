05ae20c9bdcd66b0e1bb890e75c034154ee27c98
Hilal2013 <hilalozkul@gmail.com>
2023-04-09 17:10:14 -0400

    updated

--- lab-06-orm-ecommerce/src/main/java/com/cydeo/entity/Product.java
+++ lab-06-orm-ecommerce/src/main/java/com/cydeo/entity/Product.java
@@ -1,18 +1,19 @@
 package com.cydeo.entity;
 
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 import java.util.List;
 
 @Entity
 @NoArgsConstructor
-@Data
-public class Product {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+@Getter
+@Setter
+public class Product extends BaseEntity {
+
     private String name;
     private Double price;
     private Integer quantity;
@@ -22,8 +23,8 @@ public class Product {
             joinColumns = @JoinColumn(name = "p_id"),
             inverseJoinColumns = @JoinColumn(name = "c_id"))
     private List<Category> categoryList;
-@OneToMany(mappedBy = "product")
-    private List<CartItem> cartItemList;
+//@OneToMany(mappedBy = "product")
+  //  private List<CartItem> cartItemList;
 
     public Product(String name, Double price, Integer quantity, Integer remaining_quantity) {
         this.name = name;
