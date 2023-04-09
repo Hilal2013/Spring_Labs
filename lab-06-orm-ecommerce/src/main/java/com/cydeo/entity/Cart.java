d92b01482e27c1e727545dcf93729d6010c61129
Hilal2013 <hilalozkul@gmail.com>
2023-04-09 17:09:49 -0400

    updated

--- lab-06-orm-ecommerce/src/main/java/com/cydeo/entity/Cart.java
+++ lab-06-orm-ecommerce/src/main/java/com/cydeo/entity/Cart.java
@@ -2,27 +2,26 @@ package com.cydeo.entity;
 
 import com.cydeo.enums.CartState;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 
 @Entity
 @NoArgsConstructor
-@Data
-public class Cart {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+@Getter
+@Setter
+public class Cart extends BaseEntity{
+
     @Enumerated(EnumType.STRING)
     private CartState cartState;
     @ManyToOne
-//@JoinColumn(name="discount_id")
     private Discount discount;
     @ManyToOne
     private Customer customer;
     @OneToOne(mappedBy = "cart")
     private Order order;
-
 
     public Cart(CartState cartState) {
         this.cartState = cartState;
