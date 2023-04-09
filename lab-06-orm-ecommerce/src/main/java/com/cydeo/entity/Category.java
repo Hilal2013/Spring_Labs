5d95fe6e76b60114b76fe4abb7fdcea4e9fffb4b
Hilal2013 <hilalozkul@gmail.com>
2023-04-09 17:09:57 -0400

    updated

--- lab-06-orm-ecommerce/src/main/java/com/cydeo/entity/Category.java
+++ lab-06-orm-ecommerce/src/main/java/com/cydeo/entity/Category.java
@@ -1,24 +1,22 @@
 package com.cydeo.entity;
 
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 import java.util.List;
 
 @Entity
 @NoArgsConstructor
-@Data
-public class Category {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-   private String name;
-   @ManyToMany(mappedBy = "categoryList")
-private List<Product> productList;
+@Getter
+@Setter
+public class Category extends BaseEntity{
 
-    public Category(String name, List<Product> productList) {
-        this.name = name;
-        this.productList = productList;
-    }
+   private String name;
+  // @ManyToMany(mappedBy = "categoryList")
+//private List<Product> productList;
+
+
 }
