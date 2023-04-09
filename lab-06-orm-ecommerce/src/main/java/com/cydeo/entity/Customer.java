7e23534cebec4e5923ee6bef0d6339af92bf3f68
Hilal2013 <hilalozkul@gmail.com>
2023-04-09 17:10:10 -0400

    updated

--- lab-06-orm-ecommerce/src/main/java/com/cydeo/entity/Customer.java
+++ lab-06-orm-ecommerce/src/main/java/com/cydeo/entity/Customer.java
@@ -1,30 +1,31 @@
 package com.cydeo.entity;
 
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 import java.util.List;
 
 @Entity
 @NoArgsConstructor
-@Data
-public class Customer {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+@Getter
+@Setter
+public class Customer extends BaseEntity{
+
     private String email;
     private String firstName;
     private String lastName;
     private String userName;
-    @OneToMany(mappedBy = "customer")
-    private List<Address> addressList;
+   // @OneToMany(mappedBy = "customer")
+   // private List<Address> addressList;
 
-    @OneToOne(mappedBy = "customer")
-    private Balance balance;
+  //  @OneToOne(mappedBy = "customer")
+  //  private Balance balance;
 
-    @OneToMany(mappedBy = "customer")
-    private List<Cart> cartList;
+  //  @OneToMany(mappedBy = "customer")
+   // private List<Cart> cartList;
 
     @OneToMany(mappedBy = "customer")
     private List<Order> orderList;
