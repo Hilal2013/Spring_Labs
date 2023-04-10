e3e4021e520463fa105746b817b839f3b469a1e1
Hilal2013 <hilalozkul@gmail.com>
2023-04-10 11:39:26 -0400

    updated

--- lab-07-orm-cinema/src/main/java/com/cydeo/entity/Genre.java
+++ lab-07-orm-cinema/src/main/java/com/cydeo/entity/Genre.java
@@ -1,24 +1,17 @@
 package com.cydeo.entity;
-import lombok.Data;
+
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
-import java.util.List;
+
 @Entity
 @NoArgsConstructor
-@Data
-public class Genre {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long Id;
+@Getter
+@Setter
+public class Genre extends BaseEntity {
 
     private String name;
-    @ManyToMany(mappedBy = "genreList")
-    private List<Movie> movieList;
 
-    public Genre(String name, List<Movie> movieList) {
-        this.name = name;
-        this.movieList = movieList;
-    }
 }
