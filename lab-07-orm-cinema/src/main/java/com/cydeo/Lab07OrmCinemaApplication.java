9f48a45dacec590f4d43e387c6e8c8d3e16330cc
Hilal2013 <hilalozkul@gmail.com>
2023-04-10 13:19:27 -0400

    updated

--- lab-07-orm-cinema/src/main/java/com/cydeo/Lab07OrmCinemaApplication.java
+++ lab-07-orm-cinema/src/main/java/com/cydeo/Lab07OrmCinemaApplication.java
@@ -1,7 +1,13 @@
 package com.cydeo;
 
+import org.flywaydb.core.Flyway;
+
+import org.flywaydb.core.api.output.MigrateResult;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class Lab07OrmCinemaApplication {
@@ -9,5 +15,8 @@ public class Lab07OrmCinemaApplication {
     public static void main(String[] args) {
         SpringApplication.run(Lab07OrmCinemaApplication.class, args);
     }
-
+	@Bean
+	public MigrateResult migrateResult(DataSource dataSource){
+		return Flyway.configure().baselineOnMigrate(true).dataSource(dataSource).load().migrate();
+	}
 }
