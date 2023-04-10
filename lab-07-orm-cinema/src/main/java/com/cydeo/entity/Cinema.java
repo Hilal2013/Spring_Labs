cdb81085f45b6dbc66a5d5b2495c4536605e1774
Hilal2013 <hilalozkul@gmail.com>
2023-04-10 11:39:13 -0400

    updated

--- lab-07-orm-cinema/src/main/java/com/cydeo/entity/Cinema.java
+++ lab-07-orm-cinema/src/main/java/com/cydeo/entity/Cinema.java
@@ -1,29 +1,23 @@
 package com.cydeo.entity;
 
-import lombok.Data;
+
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 import java.util.List;
 
 @Entity
 @NoArgsConstructor
-@Data
-public class Cinema {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+@Getter
+@Setter
+public class Cinema extends BaseEntity{
+
     private String name;
     private String sponsoredName;
-@OneToMany(mappedBy = "cinema")
-private List<MovieCinema> movieCinemaList;
 
-@ManyToOne
-@JoinColumn(name = "location_id")
+@ManyToOne(fetch=FetchType.LAZY)
 private Location location;
 
-    public Cinema(String name, String sponsoredName) {
-        this.name = name;
-        this.sponsoredName = sponsoredName;
-    }
 }
