c60377491d32c44b90517269034c5abb04f25449
Hilal2013 <hilalozkul@gmail.com>
2023-04-10 11:39:44 -0400

    updated

--- lab-07-orm-cinema/src/main/java/com/cydeo/entity/Movie.java
+++ lab-07-orm-cinema/src/main/java/com/cydeo/entity/Movie.java
@@ -1,16 +1,16 @@
 package com.cydeo.entity;
 
-import com.cydeo.enums.State;
-import com.cydeo.enums.Type;
-import lombok.Data;
+import com.cydeo.enums.MovieState;
+import com.cydeo.enums.MovieType;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
 import java.math.BigDecimal;
-import java.time.LocalDateTime;
-import java.util.List;
+import java.time.LocalDate;
+
+import java.util.Set;
 
 @Entity
 @NoArgsConstructor
@@ -20,28 +20,19 @@ public class Movie extends BaseEntity{
 
     private String name;
     @Enumerated(EnumType.STRING)
-    private State state;
+    private MovieState state;
     @Enumerated(EnumType.STRING)
-    private Type type;
-    @Column(columnDefinition = "TIMESTAMP")
-    private LocalDateTime releaseDate;
+    private MovieType type;
+    @Column(columnDefinition = "DATE")
+    private LocalDate releaseDate;
     private BigDecimal price;
     private Integer duration;
+    @Column(columnDefinition = "text")//you can put as much as character
     private String summary;
-    @OneToMany(mappedBy = "movie")
-    private List<MovieCinema> movieCinemaList;
     @ManyToMany
     @JoinTable(name = "MovieGenreRel",
             joinColumns = @JoinColumn(name = "movie_id"),
             inverseJoinColumns = @JoinColumn(name = "genre_id"))
-    private List<Genre> genreList;
-    public Movie(String name, State state, Type type, LocalDateTime releaseDate, BigDecimal price, Integer duration, String summary) {
-        this.name = name;
-        this.state = state;
-        this.type = type;
-        this.releaseDate = releaseDate;
-        this.price = price;
-        this.duration = duration;
-        this.summary = summary;
-    }
+    private Set<Genre> genreList;
+
 }
