d8f98a6a62b9b8208fd2337c28cb95e158b523b0
Hilal2013 <hilalozkul@gmail.com>
2023-04-12 23:00:34 -0400

    updated

--- lab-07-orm-cinema/src/main/java/com/cydeo/repository/CinemaRepository.java
+++ lab-07-orm-cinema/src/main/java/com/cydeo/repository/CinemaRepository.java
@@ -4,6 +4,8 @@ import com.cydeo.entity.Cinema;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CinemaRepository extends JpaRepository<Cinema, Long> {
 
@@ -11,10 +13,10 @@ public interface CinemaRepository extends JpaRepository<Cinema, Long> {
 
     //Write a derived query to get cinema with a specific name
 
-
+List<Cinema> findByName(String name);
     //Write a derived query to read sorted the top 3 cinemas that contains a specific sponsored name
 
-
+    List<Cinema> findTop3OrderByNameBySponsoredNameContains(String sponsoredName);
     //Write a derived query to list all cinemas in a specific country
 
 
