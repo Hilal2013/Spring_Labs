fc2a5d101a9bb720eb8d051498a9b1219b9abb33
Hilal2013 <hilalozkul@gmail.com>
2023-04-12 23:00:41 -0400

    updated

--- lab-07-orm-cinema/src/main/java/com/cydeo/repository/AccountRepository.java
+++ lab-07-orm-cinema/src/main/java/com/cydeo/repository/AccountRepository.java
@@ -4,6 +4,8 @@ import com.cydeo.entity.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
 
@@ -11,9 +13,10 @@ public interface AccountRepository extends JpaRepository<Account, Long> {
 
     //Write a derived query to list all accounts with a specific country or state
 
+List<Account> findByCountryOrState(String country, String state);
 
     //Write a derived query to list all accounts with age lower than or equal to a specific value
-
+    List<Account> findByAgeLessThanEqual(Integer age);
 
     //Write a derived query to list all accounts with a specific role
 
