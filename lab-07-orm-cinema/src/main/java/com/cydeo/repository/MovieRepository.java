43b68df0a7496488dd632a6404c94ffa1ca78b7d
Hilal2013 <hilalozkul@gmail.com>
2023-04-15 17:53:30 -0400

    updated

--- lab-07-orm-cinema/src/main/java/com/cydeo/repository/MovieRepository.java
+++ lab-07-orm-cinema/src/main/java/com/cydeo/repository/MovieRepository.java
@@ -17,18 +17,15 @@ public interface MovieRepository extends JpaRepository<Movie, Long> {
     // ------------------- DERIVED QUERIES ------------------- //
 
     //Write a derived query to read a movie with a name
-
-
     List<Movie> findByName(String name);
-
 
     //Write a derived query to list all movies between a range of prices
     List<Movie> findByPriceBetweenOrderByPrice(BigDecimal num1,BigDecimal num2);
     //Write a derived query to list all movies where duration exists in the specific list of duration
-  //  List<Movie> findByDuration(List<Integer> duration);
+
+ //  List<Movie> findByDuration(List<Integer> duration);
 
     //Write a derived query to list all movies with higher than a specific release date
-
 List<Movie> findByReleaseDateGreaterThan(LocalDate releaseDate);
     //Write a derived query to list all movies with a specific state and type
     List<Movie> findByStateAndType(MovieState state, MovieType type);
@@ -49,13 +46,13 @@ List<Movie> findByReleaseDateGreaterThan(LocalDate releaseDate);
       List<Movie> readMovieName(String name);
 
     //Write a native query that return the list of movies in a specific range of prices
-    @Query(value="select  * from movie where price between ?1 and ?2",nativeQuery = true)
-    List<Movie> readByPriceBetween(BigDecimal num1,BigDecimal num2);
+    @Query(value="select  * from movie where price between ?1 and ?2 order by price desc",nativeQuery = true)
+   List<Movie> readByPriceBetween(BigDecimal num1,BigDecimal num2);
 
     //Write a native query to return all movies where duration exists in the range of duration
-
-
+@Query(value="select * from movie where duration between ?1 and ?2",nativeQuery = true)
+    List<Movie> getMoviesDurationRange(Integer num1, Integer num2);
     //Write a native query to list the top 5 most expensive movies
-
-
+@Query(value="select name from movie order by price desc limit 5",nativeQuery = true)
+    List<String> listTopMovies();
 }
