23baab89a1d611dee2756231eb3f982956483fe1
Hilal2013 <hilalozkul@gmail.com>
2023-04-16 10:29:24 -0400

    updated

--- lab-06-orm-ecommerce/src/main/java/com/cydeo/entity/Payment.java
+++ lab-06-orm-ecommerce/src/main/java/com/cydeo/entity/Payment.java
@@ -1,26 +1,23 @@
 package com.cydeo.entity;
 
+import com.cydeo.enums.PaymentMethod;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.math.BigDecimal;
 
 @Entity
 @NoArgsConstructor
-@Data
-public class Payment {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private BigDecimal paidPrice;
-    private String paymentMethod;
+@Getter
+@Setter
+public class Payment extends BaseEntity{
 
-    public Payment(BigDecimal paidPrice, String paymentMethod) {
-        this.paidPrice = paidPrice;
-        this.paymentMethod = paymentMethod;
-    }
+    private BigDecimal paidPrice;
+    @Enumerated(EnumType.STRING)
+    private PaymentMethod paymentMethod;
+
+
 }
