a64b30f949699b3f6090c71a04ffa2a40307eb0f
Hilal2013 <hilalozkul@gmail.com>
2023-04-22 19:28:25 -0400

    updated

--- lab-07-orm-cinema/src/main/java/com/cydeo/repository/UserRepository.java
+++ lab-07-orm-cinema/src/main/java/com/cydeo/repository/UserRepository.java
@@ -3,6 +3,7 @@ package com.cydeo.repository;
 import com.cydeo.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -30,10 +31,10 @@ List<User>  findByUsernameContaining(String pattern);
 
     //Write a JPQL query that returns a user read by email?
 @Query("select u from User u where u.email=?1")
-    User  getUserMail(String email);
+    User  getUserMail(@Param("email")String email);
     //Write a JPQL query that returns a user read by username?
     @Query("select u from User u where u.username=?1")
-    User  getUserUserName(String userName);
+    User  getUserUserName(@Param("username")String userName);
 
     //Write a JPQL query that returns all users?
     @Query("select u from User u ")
@@ -42,15 +43,15 @@ List<User>  findByUsernameContaining(String pattern);
     // ------------------- Native QUERIES ------------------- //
 
     //Write a native query that returns all users that contain a specific name?
-//@Query(value="select * from user_account where user like%?1% ",nativeQuery = true)
-//List<User> returnUsersContainName(String pattern);
+@Query(value="select * from user_account where username ilike concat('%',?1,'%')",nativeQuery = true)
+List<User> returnUsersContainName(@Param("pattern")String pattern);
     //Write a native query that returns all users?
     @Query(value="select * from user_account ",nativeQuery = true)
     List<User> returnUsers();
     //Write a native query that returns all users in the range of ages?
-//@Query(value="",nativeQuery = true)
- //   List<User> returnUsersAge();
+@Query(value="select * from user_account u join account_details a on u.accoun_details_id=a.id where age between ?1 and ?2 ",nativeQuery = true)
+   List<User> returnUsersAge(@Param("age1")Integer age1,@Param("age2")Integer age2);
     //Write a native query to read a user by email?
-
-
+@Query(value="select * from user_account where email=?1 ",nativeQuery = true)
+User readUserByEmail(@Param("email")String email);
 }
