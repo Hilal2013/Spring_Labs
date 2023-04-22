aa7d04b1095245d9cb11e45aaa0cbb761b9933f2
Hilal2013 <hilalozkul@gmail.com>
2023-04-22 19:28:31 -0400

    updated

--- lab-07-orm-cinema/src/main/java/com/cydeo/repository/TicketRepository.java
+++ lab-07-orm-cinema/src/main/java/com/cydeo/repository/TicketRepository.java
@@ -3,6 +3,7 @@ package com.cydeo.repository;
 import com.cydeo.entity.Ticket;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
@@ -29,30 +30,36 @@ public interface TicketRepository extends JpaRepository<Ticket, Long> {
 
     //Write a JPQL query that returns all tickets are bought from a specific user
 @Query("select t from Ticket t where t.userAccount.id=?1")
-    List<Ticket> getTicketUser(Long id);
+    List<Ticket> getTicketUser( @Param("id")Long id);
     //Write a JPQL query that returns all tickets between a range of dates
 @Query("select t from Ticket t where t.dateTime between ?1 and ?2")
-    List<Ticket> getTicketsDatesBetween(LocalDateTime start, LocalDateTime end);
+    List<Ticket> getTicketsDatesBetween(@Param("startDate")LocalDateTime startDate,@Param("endDate") LocalDateTime endDate);
     // ------------------- Native QUERIES ------------------- //
 
     //Write a native query to count the number of tickets a user bought
-  // @Query(value="select count(t) from ticket t join user_account u on t.id=u.id where t.u.id=?1",nativeQuery = true)
- //   int countUserAccountId(Long id);
+   @Query(value="select count(*) from ticket t join user_account u on t.user_acoount_id=u.id where u.id=?1",nativeQuery = true)
+    int countUserAccountId(@Param("id")Long id);
 
 
     //Write a native query to count the number of tickets a user bought in a specific range of dates
-
+    @Query(value=" select count(*) from ticket  where datetime between ?1 and ?2", nativeQuery = true)
+    int countTicketsUserDate(@Param("startDate")LocalDateTime startDate,@Param("endDate")LocalDateTime endDate);
 
     //Write a native query to distinct all tickets by movie name
 
-
+@Query(value="select distinct m.name  from ticket t join movie_cinema mc on t.movie_cinema_id=mc.id join movie m on  ",nativeQuery = true)
+    List<Ticket> distinctTicketsMovieName();
     //Write a native query to find all tickets by user email
-
+    @Query(value="select * from ticket t join user_account u on t.user_acoount_id=u.id where u.username=?1",nativeQuery = true)
+    List<Ticket> getTicketsUserEmail(@Param("username")String username);
 
     //Write a native query that returns all tickets
-
-
+    @Query(value="select * from ticket",nativeQuery = true)
+    List<Ticket> getAllTickets();
     //Write a native query to list all tickets where a specific value should be containable in the username or account name or movie name
 
-
+    @Query(value=" select * from ticket t join user_account u on t.user_acoount_id=u.id " +
+            "join account_details a on u.accoun_details_id=a.id join movie_cinema mc on t.movie_cinema_id=mc.id join movie m on mc.movie_id=m.id where u.username ILIKE concat('%',?1,'%')" +
+            "OR a.name account_details OR m.name account_details ILIKE concat('%',?1,'%') ",nativeQuery = true)
+    List<Ticket> allTicketsContainsValue(@Param("pattern") String pattern);
 }
