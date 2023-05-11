2442494502e935bb7bacae1778c50ac0353091e8
Hilal2013 <hilalozkul@gmail.com>
2023-05-10 21:24:58 -0400

    CartItem

---
+++ lab-07-orm-queries/src/main/java/com/cydeo/entity/CartItem.java
@@ -0,0 +1,23 @@
+package com.cydeo.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.ManyToOne;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class CartItem extends BaseEntity {
+
+    private Integer quantity;
+    @ManyToOne
+    private Cart cart;
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Product product;
+
+}
