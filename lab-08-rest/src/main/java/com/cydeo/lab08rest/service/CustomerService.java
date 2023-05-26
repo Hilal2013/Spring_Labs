6718e2c3e0a51f7e7b36f7ae7541045acb728561
Hilal2013 <hilalozkul@gmail.com>
2023-05-26 19:56:34 -0400

    updated

--- lab-08-rest/src/main/java/com/cydeo/lab08rest/service/CustomerService.java
+++ lab-08-rest/src/main/java/com/cydeo/lab08rest/service/CustomerService.java
@@ -1,4 +1,16 @@
 package com.cydeo.lab08rest.service;
 
+import com.cydeo.lab08rest.dto.AddressDTO;
+import com.cydeo.lab08rest.dto.CustomerDTO;
+
+import java.util.List;
+
 public interface CustomerService {
+
+
+    List<CustomerDTO> findAllCustomers();
+    CustomerDTO findCustomerEmail(String email);
+
+    CustomerDTO save(CustomerDTO customer);
+    void update(CustomerDTO customer);
 }
