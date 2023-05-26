7d4c05b066178b8fc7dd5155ed680564fd06b59f
Hilal2013 <hilalozkul@gmail.com>
2023-05-26 19:56:39 -0400

    updated

--- lab-08-rest/src/main/java/com/cydeo/lab08rest/controller/CustomerController.java
+++ lab-08-rest/src/main/java/com/cydeo/lab08rest/controller/CustomerController.java
@@ -1,4 +1,45 @@
 package com.cydeo.lab08rest.controller;
 
+import com.cydeo.lab08rest.dto.AddressDTO;
+import com.cydeo.lab08rest.dto.CustomerDTO;
+import com.cydeo.lab08rest.model.ResponseWrapper;
+import com.cydeo.lab08rest.service.CustomerService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/customer")
 public class CustomerController {
+
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    private final CustomerService customerService;
+@GetMapping
+    public ResponseEntity<ResponseWrapper> getCustomerList(){
+    return ResponseEntity.ok(new ResponseWrapper("CustomerList Retrieved", customerService.findAllCustomers(), HttpStatus.OK));
+
+    }
+@GetMapping("/{email}")
+    public  ResponseEntity<ResponseWrapper> getCustomerListByEmail(@PathVariable("email") String email){
+
+    return ResponseEntity.ok(new ResponseWrapper("Customer Retrieved", customerService.findCustomerEmail(email), HttpStatus.OK));
+
+    }
+
+@PostMapping
+    public  ResponseEntity<ResponseWrapper> createCustomer(@RequestBody CustomerDTO customer){
+
+    return ResponseEntity.ok(new ResponseWrapper("Customer Saved",  customerService.save(customer), HttpStatus.OK));
+
+    }
+
+    @PutMapping
+    public  ResponseEntity<Void> updateCustomer(@RequestBody CustomerDTO customer){
+customerService.update(customer);
+        return ResponseEntity.noContent().build();
+    }
+
 }
