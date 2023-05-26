c5ba5a7500bec186e5e71c54e9724f255a393b67
Hilal2013 <hilalozkul@gmail.com>
2023-05-26 17:57:24 -0400

    updated

--- lab-08-rest/src/main/java/com/cydeo/lab08rest/service/AddressService.java
+++ lab-08-rest/src/main/java/com/cydeo/lab08rest/service/AddressService.java
@@ -1,4 +1,16 @@
 package com.cydeo.lab08rest.service;
 
+import com.cydeo.lab08rest.dto.AddressDTO;
+import com.cydeo.lab08rest.entity.Address;
+import com.cydeo.lab08rest.entity.Customer;
+
+import java.util.List;
+
 public interface AddressService {
+List<AddressDTO> findAllAddress();
+    List<AddressDTO> findAddressCustomerId(Long id);
+    List<AddressDTO> findAddressCustomerAndName(Long id,String name);
+    List<AddressDTO> findAddressStartingWith(String keyword);
+    AddressDTO save(AddressDTO address);
+    void update(AddressDTO address);
 }
