57773b59af9d14bc339c36193dc6edf478103daa
Hilal2013 <hilalozkul@gmail.com>
2023-05-26 13:52:42 -0400

    created lab08rest module

---
+++ lab-08-rest/src/main/java/com/cydeo/lab08rest/Lab08RestApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo.lab08rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab08RestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab08RestApplication.class, args);
+    }
+
+}
