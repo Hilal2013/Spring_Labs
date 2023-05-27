3ed3acff2ab299c6fe73b6c2e438b515cc324bbc
Hilal2013 <hilalozkul@gmail.com>
2023-05-27 17:45:02 -0400

    OrderService

---
+++ lab-08-rest/src/main/java/com/cydeo/lab08rest/service/OrderService.java
@@ -0,0 +1,15 @@
+package com.cydeo.lab08rest.service;
+
+import com.cydeo.lab08rest.dto.OrderDTO;
+import com.cydeo.lab08rest.entity.Order;
+import com.cydeo.lab08rest.enums.PaymentMethod;
+
+import java.util.List;
+
+public interface OrderService {
+    List<OrderDTO> findAllOrder();
+    List<OrderDTO> findAllByCustomerEmail(String email);
+    List<OrderDTO> findAllByPayment_PaymentMethod(PaymentMethod paymentMethod);
+    OrderDTO save(OrderDTO orderDTO);
+    void update(OrderDTO orderDTO);
+}
