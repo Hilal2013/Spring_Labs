6702d5be5642495848734fbc6b3fa7a062b09ab9
Hilal2013 <hilalozkul@gmail.com>
2023-06-01 19:14:43 -0400

    DTOs

---
+++ lab-08-rest-SchoolLab/src/main/java/com/cydeo/dto/ResponseWrapper.java
@@ -0,0 +1,34 @@
+package com.cydeo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResponseWrapper {
+    private boolean success;
+    private String message;
+    private Integer code;
+    private Object data;
+
+    public ResponseWrapper(String message, Object data) {
+        this.success = true;
+        this.code = HttpStatus.OK.value();//200
+        this.message = message;
+
+        this.data = data;
+    }
+
+    public ResponseWrapper(String message) {
+        this.success = true;
+        this.message = message;
+        this.code = HttpStatus.OK.value();
+    }
+
+
+}
