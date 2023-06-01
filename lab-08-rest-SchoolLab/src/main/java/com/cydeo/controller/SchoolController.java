be58bc100a7dce2d6b24a3c350b4b5bf30c3d021
Hilal2013 <hilalozkul@gmail.com>
2023-06-01 19:50:48 -0400

    SchoolController

---
+++ lab-08-rest-SchoolLab/src/main/java/com/cydeo/controller/SchoolController.java
@@ -0,0 +1,31 @@
+package com.cydeo.controller;
+
+import com.cydeo.dto.TeacherDTO;
+import com.cydeo.service.AddressService;
+import com.cydeo.service.ParentService;
+import com.cydeo.service.StudentService;
+import com.cydeo.service.TeacherService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class SchoolController {
+    private final TeacherService teacherService;
+    private final StudentService studentService;
+    private final ParentService parentService;
+    private final AddressService addressService;
+
+    public SchoolController(TeacherService teacherService, StudentService studentService, ParentService parentService, AddressService addressService) {
+        this.teacherService = teacherService;
+        this.studentService = studentService;
+        this.parentService = parentService;
+        this.addressService = addressService;
+    }
+    @GetMapping("/teachers")
+    public List<TeacherDTO> getTeachers() {
+
+        return teacherService.findAll();
+    }
+}
