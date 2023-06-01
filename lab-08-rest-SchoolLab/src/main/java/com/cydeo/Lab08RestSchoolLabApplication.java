a086a319b85d406b0f15cf5598a27962c53e10af
Hilal2013 <hilalozkul@gmail.com>
2023-06-01 19:20:39 -0400

    updated

--- lab-08-rest-SchoolLab/src/main/java/com/cydeo/Lab08RestSchoolLabApplication.java
+++ lab-08-rest-SchoolLab/src/main/java/com/cydeo/Lab08RestSchoolLabApplication.java
@@ -1,7 +1,9 @@
 package com.cydeo;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Lab08RestSchoolLabApplication {
@@ -9,5 +11,10 @@ public class Lab08RestSchoolLabApplication {
     public static void main(String[] args) {
         SpringApplication.run(Lab08RestSchoolLabApplication.class, args);
     }
+    @Bean
+    public ModelMapper mapper() {
+        return new ModelMapper();
+    }
 
 }
+
