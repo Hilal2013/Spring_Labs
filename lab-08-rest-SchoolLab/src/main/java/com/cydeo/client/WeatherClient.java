d72a9880fb8e43c9c731b9886bdf37474a38caad
Hilal2013 <hilalozkul@gmail.com>
2023-06-01 22:07:46 -0400

    updated

--- lab-08-rest-SchoolLab/src/main/java/com/cydeo/client/WeatherClient.java
+++ lab-08-rest-SchoolLab/src/main/java/com/cydeo/client/WeatherClient.java
@@ -1,12 +1,14 @@
 package com.cydeo.client;
 
+import com.cydeo.dto.WeatherDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(url="https://weatherstack.com/",name = "WEATHER-CLIENT")
 public interface WeatherClient {
 
-   // @GetMapping("")
-  //  WeatherDTO
+    @GetMapping("/current")//from weatherstack website
+    WeatherDTO getCurrentWeather(@RequestParam(value = "accessKey") String accessKey);
 
 }
